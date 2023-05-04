5126daa7ec9ef2ebead95e129d545da6faee7626
Ezequias Soares <87997012+kia735@users.noreply.github.com>
2023-05-03 23:45:44 -0300

    Edição de Papeis Correção

--- src/screens/updateUser/UpdateUser.js
+++ src/screens/updateUser/UpdateUser.js
@@ -2,15 +2,9 @@ import React from 'react';
 import './UpdateUser.css';
 import '../../components/Style.css';
 import { withRouter } from 'react-router-dom';
-//import axios from 'axios';
 import UserApiService from '../../services/UserApiService';
-
 import Card from '../../components/Card';
 import FormGroup from '../../components/FormGroup';
-import SelectDepartament from '../../components/SelectDepartament';
-
-import DepartamentApiService from '../../services/DepartamentApiService';
-
 import { showSuccessMessage, showErrorMessage } from '../../components/Toastr';
 
 
@@ -22,148 +16,75 @@ class UpdateUser extends React.Component {
         email: '',
         username: '',
         rolesName:'',
-        // role: '',
-        password: '',
-        // username: '',
-        // departament: {
-        //     departamentId: 0,
-        //     name: ''
-        // } 
-
+        password: ''
     }
+
     constructor() {
         super();
         this.service = new UserApiService();
-        
     }
 
     componentDidMount() {
-       
         const params = this.props.match.params;
         const id = params.id;
         this.findById(id);
-        this.showEditRole();
-       
-       
     }
    
-    showEditRole = () =>{
-        var value =  localStorage.getItem("user");
-        var user = JSON.parse(value)
-        var role = user['roles']['0']['name']
-
-        console.log("AA", user)
-        let a
-        if(role === 'ADMIN'){
-            a = document.getElementById("papel")
-            a.classList.add('show')
-           console.log("Foi", role)
-        }       
-       
-    }
-
     findById = (id) => {
         this.service.find(`?id=${id}`)
             .then(response => {
-                console.log(response);
                 const user = response.data; 
-                
                 const id = user[0].id;
                 const name = user[0].name;
                 const email = user[0].email;
-                // const registration = user.registration;
-                // const role = user.role;
-                // const password = user.password;
-                // const departament = user.departament;
                 const rolesName = user[0]['roles'][0]['name'];
                 const username = user[0].username;
                 const password = user[0].password;
 
-                console.log("Dados up", user)
-                console.log("Papel  nome", user[0]['roles'][0]['name'])
-
                 this.setState({ id:id, name:name, email:email, username:username, rolesName:rolesName, password:password}); 
-
-
                 this.showPapel(rolesName);               
-
             }            
 
             ).catch(error => {
-                console.log(error.response);
+               showErrorMessage("Erro. Tente novamente")
                 console.log(error.message);
             }
             );
             
     }
 
+    setSelectedRole() {
+        const selectElement = document.getElementById("selectRole");
+        const selectedValue = selectElement.value;
+        this.setState({rolesName: selectedValue}) ;
+        console.log(this.state.rolesName)
+    }
+
     showPapel(role){
-        if(role === 'ADMIN'){         
-
-           const seletor = document.getElementById('ADMIN')
-            seletor.selected = true;
-
-           
-        }else if (role === 'STUDENTS'){
-            const seletor = document.getElementById('STUDENTS')
-            seletor.selected = true;
+        let papel = document.getElementById("papel");
+        let seletor = document.getElementById(role);   
+        seletor.selected = true;
+        papel.classList.add('show')
+        
+        const user = JSON.parse(localStorage.getItem("loggedUser"));
+        if(user.roles[0].name != 'ADMIN'){
+            document.getElementById("selectRole").disabled = true;      
         }
     }
 
-
-    // validate = () => {
-    //     const errors = [];
-
-    //     if (!this.state.name) {
-    //         errors.push('Campo Nome é obrigatório!');
-    //     } else if(!this.state.name.match(/[A-z ]{2,50}$/)) {
-    //         errors.push('O Nome deve ter no mínimo 2 e no máximo 50 caracteres!');
-    //     }
-
-    //     if (!this.state.email) {
-    //         errors.push('Campo E-mail é obrigatório!');
-    //     } else if (!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
-    //         errors.push('Informe um E-mail válido!');
-    //     }
-
-    //     if (!this.state.registration) {
-    //         errors.push('Campo Matrícula é obrigatório!');
-    //     }
-
-    //     if (!this.state.role) {
-    //         errors.push('É obrigatório informar o Papel!');
-    //     }
-
-    //     if (!this.state.password) {
-    //         errors.push('Campo Senha é obrigatório!')
-    //     } else if(!this.state.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,30}$/)) {
-    //         errors.push('A Senha deve ter no mínimo 8 e no máximo 30 caracteres.')
-    //         errors.push('A Senha deve conter ao menos um número.')
-    //         errors.push('A Senha deve conter ao menos uma letra minúscula.')
-    //         errors.push('A Senha deve conter ao menos uma letra maiúscula.')
-    //         errors.push('A Senha deve conter ao menos um caractere especial.')
-    //     }
-
-    //     // if (!this.state.departamentId) {
-    //     //     errors.push('É obrigatório informar o Departamento!');
-    //     // }
-
-    //     return errors;
-    // };
-
     update = () => {
+       const user ={      
+        "name":this.state.name,
+        "email":this.state.email,
+        "password":this.state.password,
+        "username": this.state.username,
+        "departamentId": 1,
+        "roles":[{"name":this.state.rolesName}]
+        }
+        console.log(user);
 
-        this.service.update(this.state.id,
-            {
-                "name":this.state.name,
-                "email":this.state.email,
-                "password":this.state.password,
-                "username": this.state.username,
-                "departamentId": 1,
-                "roles":[{"name":this.state.rolesName}]
-               
-            }
-        ).then(response => {
+        this.service.update(this.state.id, user)
+        .then(response => {
             console.log(response);
             showSuccessMessage('Usuário atualizado com sucesso!');
             this.props.history.push("/viewUsers");
@@ -199,11 +120,7 @@ class UpdateUser extends React.Component {
                                                             * Todos os campos são obrigatórios.
                                                         </small>
                                                     </p>
-                                                    {/* <FormGroup label="Id: *" htmlFor="inputUserId">
-                                                        <input type="number" id="inputUserId" disabled={true} className="form-control"
-                                                            value={this.state.id} name="id" onChange={(e) => { this.setState({ id: e.target.value }) }} />
-                                                    </FormGroup>
-                                                    <br /> */}
+            
                                                     <FormGroup label="Nome:" htmlFor="inputUserName">
                                                         <input disabled type="text" id="inputUserName" className="form-control" 
                                                             value={this.state.name} name="name"  />
@@ -214,7 +131,6 @@ class UpdateUser extends React.Component {
                                                          id="inputEmail" 
                                                          className="form-control" 
                                                             value={this.state.email} name="email" onChange={(e) => { this.setState({ email: e.target.value }) }} />
-                                                        {/* <small id="emailHelp" className="form-text text-muted">É obrigatório o uso do e-mail acadêmico.</small> */}
                                                     </FormGroup>
                                                     <br />
                                                     <FormGroup label="Matrícula:" htmlFor="inputRegistration">
@@ -225,34 +141,16 @@ class UpdateUser extends React.Component {
                                                     <div id='papel' className="form-group roles">
                                                         <label htmlFor="selectRole" className="form-label mt-4">Papel: *</label>
                                                         
-                                                        <select className="form-select" id="selectRole"                            name="role" onChange={(e) =>{this.setState({rolesName: e.target.value})}}>
-
-                                                            <option>Selecione uma opção</option>
-
-                                                           
-                                                            <option id='ADMIN'
-                                                            value="ADMIN">ADMINISTRADOR</option>
-
-                                                            
-                                                            <option id='STUDENTS'  value="STUDENTS" >ESTUDANTE</option>
-                                                            
-                                                            
-                                                            <option value="TECHNICIAN">TÉCNICO</option>
-                                                            <option value="TEACHER">PROFESSOR</option>
+                                                        <select className="form-select" id="selectRole" name="role" onChange={() =>{this.setSelectedRole()}}>
+                                                    
+                                                            <option id='ADMIN' value="ADMIN">ADMINISTRADOR</option>
+                                                            <option id='STUDENTS'  value="STUDENTS" >ESTUDANTE</option>                                                         
+                                                            <option id='TECHNICIAN' value="TECHNICIAN">TÉCNICO</option>
+                                                            <option id='TEACHER' value="TEACHER">PROFESSOR</option>
                                                             
                                                         </select>
                                                     </div>
                                                     <br />
-                                                    {/* <FormGroup label="Senha: *" htmlFor="inputPassword">
-                                                        <input type="password" id="inputPassword" className="form-control"
-                                                            value={this.state.password} name="password" onChange={(e) => { this.setState({ password: e.target.value }) }} />
-                                                        <small id="passwordHelp" className="form-text text-muted">A senha deve ter no mínimo 8 e no máximo 30 caracteres.</small>
-                                                    </FormGroup>
-                                                    <br /> */}
-                                                    {/* <FormGroup label="Departamento: *" htmlFor="inputDepartamentId">
-                                                        <br />
-                                                        <SelectDepartament id="select-departament" onChange={this.inputSelectDepartament} />
-                                                    </FormGroup> */}
                                                     <br />
                                                     <br />
                                                     <button onClick={this.update} type="button" id="button-save" className="btn btn-success">
